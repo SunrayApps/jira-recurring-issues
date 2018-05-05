09c96402292e338c3cd4d077de775bad6f1732cd
Michal <michal@sunrayapps.com>
2018-05-05 22:39:40 +0200

    Add period field to the create issue form

--- src/main/resources/js/recurring-issues.js
+++ src/main/resources/js/recurring-issues.js
@@ -1,5 +1,26 @@
-var createIssueForm = JIRA.Templates.Issue.createIssueForm;
+if (typeof SunrayApps == 'undefined') { var SunrayApps = {}; }
+if (typeof SunrayApps.recurringIssues == 'undefined') { SunrayApps.recurringIssues = {}; }
+
+SunrayApps.recurringIssues.createIssueForm = JIRA.Templates.Issue.createIssueForm,
+SunrayApps.recurringIssues.toggleRecurringIssuesFields = function(checkbox) {
+     if(AJS.$("#recurring-issues-period").size() == 0){
+        var periodFieldTemplate = SunrayApps.Templates.RecurringIssues.periodField()
+        AJS.$(".issue-setup-fields").append(periodFieldTemplate);
+     }
+     AJS.$("#is-recurring-issue").val(checkbox.checked);
+     var periodField = AJS.$("#recurring-issues-period")
+     if(checkbox.checked){
+        periodField.parent().show();
+        periodField.focus();
+     }else{
+        periodField.parent().hide();
+     }
+}
+
 JIRA.Templates.Issue.createIssueForm = function(opt_data, opt_ignored){
-    var issueForm = createIssueForm(opt_data, opt_ignored);
-    return issueForm.replace('<div class="buttons">','<div class="buttons"><label for="qf-create-recurring" class="qf-create-recurring"><input id="qf-create-recurring" type="checkbox">Create recurring</label>');
+    var issueForm = SunrayApps.recurringIssues.createIssueForm(opt_data, opt_ignored);
+    return issueForm.replace(
+        '<div class="buttons">',
+        SunrayApps.Templates.RecurringIssues.checkbox()
+    );
 }
