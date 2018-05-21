1ee46e5e483186d098fd6f09e85e160b84d3f537
Michal <michal@sunrayapps.com>
2018-05-21 21:43:01 +0200

    Test if create issue dialog creates issues
    I wouldn't like to break a standard way user creates issues in Jira.

--- src/test/java/it/com/sunrayapps/jira/plugin/ir/CreateRecurringIssueTest.java
+++ src/test/java/it/com/sunrayapps/jira/plugin/ir/CreateRecurringIssueTest.java
@@ -1,6 +1,8 @@
 package it.com.sunrayapps.jira.plugin.ir;
 
 import com.atlassian.jira.pageobjects.JiraTestedProduct;
+import com.atlassian.jira.testkit.client.restclient.SearchClient;
+import com.atlassian.jira.testkit.client.restclient.SearchRequest;
 import it.com.sunrayapps.jira.plugin.ir.po.ExtendedCreateIssuePage;
 import it.com.sunrayapps.jira.plugin.ir.po.TopMenuPage;
 import it.com.sunrayapps.jira.plugin.ir.rule.JiraIntegrationTestRule;
@@ -20,5 +22,23 @@ public class CreateRecurringIssueTest {
         final ExtendedCreateIssuePage createIssuePage = topMenuPage.createIssue();
 
         Assert.assertTrue(createIssuePage.isCrateRecurringCheckboxVisible());
+    }
+
+    @Test
+    public void shouldCreateStandardIssue() {
+        jira.quickLoginAsAdmin();
+        final SearchClient search = jira
+            .backdoor()
+            .search();
+        final Integer issuesBefore = search.getSearch(new SearchRequest()).total;
+
+        final TopMenuPage topMenuPage = jira.goTo(TopMenuPage.class);
+        final ExtendedCreateIssuePage createIssuePage = topMenuPage.createIssue();
+        createIssuePage.fillSummary("A new issue");
+        createIssuePage.submit();
+
+        final Integer issuesAfter = search.getSearch(new SearchRequest()).total;
+
+        Assert.assertTrue(issuesAfter == issuesBefore + 1);
     }
 }
--- src/test/java/it/com/sunrayapps/jira/plugin/ir/po/ExtendedCreateIssuePage.java
+++ src/test/java/it/com/sunrayapps/jira/plugin/ir/po/ExtendedCreateIssuePage.java
@@ -4,6 +4,7 @@ import com.atlassian.jira.pageobjects.pages.AbstractJiraPage;
 import com.atlassian.pageobjects.elements.ElementBy;
 import com.atlassian.pageobjects.elements.PageElement;
 import com.atlassian.pageobjects.elements.query.TimedCondition;
+import org.openqa.selenium.By;
 
 public class ExtendedCreateIssuePage extends AbstractJiraPage {
     @ElementBy(
@@ -16,6 +17,24 @@ public class ExtendedCreateIssuePage extends AbstractJiraPage {
     )
     private PageElement createIssueSubmit;
 
+    @ElementBy(
+        id = "summary"
+    )
+    private PageElement summary;
+
+    @ElementBy(
+        id = "create-issue-submit"
+    )
+    private PageElement submit;
+
+    public void fillSummary(String summaryValue) {
+        summary.type(summaryValue);
+    }
+
+    public void submit() {
+        submit.click();
+        driver.waitUntilElementIsNotVisible(By.id("create-issue-dialog"));
+    }
 
     public boolean isCrateRecurringCheckboxVisible() {
         return createRecurringCheckbox.isVisible();
